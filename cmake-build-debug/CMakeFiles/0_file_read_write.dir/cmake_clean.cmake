c6b2decf3940cb3f0a632a56f5b769d61d32a1f1
zhutoushaonian <761228261@qq.com>
2021-04-01 23:43:08 +0800

    update config

---
+++ cmake-build-debug/CMakeFiles/0_file_read_write.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0_file_read_write.exe"
+  "0_file_read_write.exe.manifest"
+  "0_file_read_write.pdb"
+  "CMakeFiles/0_file_read_write.dir/unix_programing/chapter3/0_file_read_write.c.obj"
+  "lib0_file_read_write.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/0_file_read_write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
